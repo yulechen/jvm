82f669d804a27f3b1cfe2367a0541bfb4187c10e
gc_chenqiu <gc_chenqiu@huoyunren.com>
2017-07-24 18:15:48 +0800

    添加直接内存设置

--- src/com/github/yulechen/jvmarg/Main.java
+++ src/com/github/yulechen/jvmarg/Main.java
@@ -1,21 +1,28 @@
 package com.github.yulechen.jvmarg;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+
+import sun.misc.Unsafe;
 
 
 public class Main {
     /**
      * JVM 参数测试 -verbose:gc -Xms32m -Xmx32m -Xmn16m -Xss128k -XX:PermSize=32m
      * -XX:MaxPermSize=32m -XX:-PrintGCDetails
+     * -XX:MaxDirectMemorySize=10M(直接内存设置)
      * 
      * @param args
      * @throws IOException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
      */
 
-    public static void main(String[] args) throws IOException {
-        testStackMemoryErrorOnJavaheapspace();
+    public static void main(String[] args) throws Exception {
+        // testStackMemoryErrorOnJavaheapspace();
+        testMaxDirectMemorySize();
         System.out.println("Hello World");
         System.in.read();
     }
@@ -30,7 +37,7 @@ public class Main {
         }
     }
 
-    private static int maxcount = 500;
+    private static int maxcount = 1000;
     private static int count = 0;
 
 
@@ -44,5 +51,20 @@ public class Main {
         else {
             return;
         }
+    }
+
+
+    public static void testMaxDirectMemorySize() throws Exception {
+        int _1M = 1024 * 1024;
+        for (int i = 0; i < 10; i++)
+            getUnsafe().allocateMemory(_1M);
+    }
+
+
+    private static Unsafe getUnsafe() throws Exception {
+        // Get the Unsafe object instance
+        Field field = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+        field.setAccessible(true);
+        return (sun.misc.Unsafe) field.get(null);
     }
 }
